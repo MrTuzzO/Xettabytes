02313cddb75419d6d7d37f1bfd3e9ead684a37a6
TuzzO <87898569+MrTuzzO@users.noreply.github.com>
2024-04-07 00:04:52 +0600

    Update Minimum_XOR.cpp

--- Week 4/Day 1/Minimum_XOR.cpp
+++ Week 4/Day 1/Minimum_XOR.cpp
@@ -5,5 +5,23 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+
+        int XOR = 0;
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+            XOR ^= arr[i];
+        }
+        int ans = XOR;
+        for (int i = 0; i < n; i++) {
+            ans = min(ans, (XOR ^ arr[i]));
+        }
+        cout << ans<<endl;
+    }
     return 0;
 }
