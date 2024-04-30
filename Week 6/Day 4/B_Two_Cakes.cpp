cd77796de5e4b0a0f2ab33a7ccc8650179482a8a
TuzzO <87898569+MrTuzzO@users.noreply.github.com>
2024-04-30 22:38:18 +0600

    Add files via upload
    Day 4 added

---
+++ Week 6/Day 4/B_Two_Cakes.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, a, b;
+    cin >> n >> a >> b;
+
+    int l = 1, r = 200, ans;
+    while (l <= r) {
+        
+    }
+
+    return 0;
+}
