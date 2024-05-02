0960334e914bfc729e7ce1884497a2b6ddb315ec
TuzzO <87898569+MrTuzzO@users.noreply.github.com>
2024-05-02 16:20:22 +0600

    Update C_Number_Game.cpp

--- Week 6/Day 4/C_Number_Game.cpp
+++ Week 6/Day 4/C_Number_Game.cpp
@@ -16,8 +16,40 @@ int main() {
             cin >> arr[i];
         }
 
-        
-    }
+        auto isOk = [&](vector<int> v, int n, int k) {
+            multiset<int> s;
+            for (int x : v) s.insert(x);
 
+            for (int i = 1; i <= k; i++) {
+                int move = k - i + 1;
+                if (s.empty()) return false;
+
+                auto it = upper_bound(s.begin(), s.end(), move);
+                if (it == s.begin()) return false;
+                else it--;
+                s.erase(it);
+
+                if(!s.empty()){
+                    int min = *s.begin();
+                    min += move;
+                    s.erase(s.begin());
+                    s.insert(min);
+                }
+            }
+            return true;
+        };
+
+        int l = 0, r = n, ans = 0;
+        while (l <= r) {
+            int k = (l + r) / 2;
+            if (isOk(arr, n, k)) {
+                ans = k;
+                l = k + 1;
+            } else {
+                r = k - 1;
+            }
+        }
+        cout << ans << endl;
+    }
     return 0;
 }
