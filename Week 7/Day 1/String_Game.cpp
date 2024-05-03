c3390a08abbcfa05d4d5df1c32193fb79e6cfda4
TuzzO <87898569+MrTuzzO@users.noreply.github.com>
2024-05-04 00:14:50 +0600

    Update String_Game.cpp

--- Week 7/Day 1/String_Game.cpp
+++ Week 7/Day 1/String_Game.cpp
@@ -5,6 +5,27 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+
+        stack<char> st;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if(!st.empty() and st.top() != s[i]){
+                st.pop();
+                count++;
+            }else{
+                st.push(s[i]);
+            }
+        }
+        if (count % 2 == 1) cout << "Zlatan\n";
+        else
+            cout << "Ramos\n";
+    }
     return 0;
 }
