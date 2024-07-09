2e28910bcc528ecb16efa27a7bd0ecc89ec37371
TuzzO <87898569+MrTuzzO@users.noreply.github.com>
2024-07-09 23:31:07 +0600

    Add files via upload

---
+++ Week 15/Day 3/B_Asterisk_Minor_Template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define NO cout << "NO\n"
+#define YES cout << "YES\n"
+// ll lcm(ll a, ll b) { return ((a / __gcd(a, b)) * b); }
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string a, b, t;
+        cin >> a >> b;
+    }
+
+    return 0;
+}
