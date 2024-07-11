1e3beecf5fd31fad389b33e75b0f59588d76b65d
TuzzO <87898569+MrTuzzO@users.noreply.github.com>
2024-07-11 17:33:05 +0600

    Update G_Even_Odd_XOR.cpp

--- Week 15/Day 4/G_Even_Odd_XOR.cpp
+++ Week 15/Day 4/G_Even_Odd_XOR.cpp
@@ -14,7 +14,34 @@ int main() {
     while (t--) {
         int n;
         cin >> n;
+        if (n % 4 == 0) {
+            for (size_t i = 2; i < n + 2; i++) {
+                cout << i << " ";
+            }
+            cout << endl;
+        } else if (n % 4 == 1) {
+            cout << "0 ";
+            for (size_t i = 2; i < n + 1; i++) {
+                cout << i << " ";
+            }
+            cout << endl;
+        } else if (n % 4 == 2) {
+            cout << "2 1 3 4 12 8 ";
+            int e = 14;
+            for (size_t i = 1; i <= n - 6; i++) {
+                cout << e << " ";
+                e++;
+            }
+            cout << endl;
+        } else {
+            cout << "2 1 3 ";
+            int e = 4;
+            for (size_t i = 1; i <= n - 3; i++) {
+                cout << e << " ";
+                e++;
+            }
+            cout << endl;
+        }
     }
-
     return 0;
 }
