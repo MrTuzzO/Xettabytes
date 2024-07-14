9f7126f8a55c729994367e0002996717fb94a6ba
TuzzO <87898569+MrTuzzO@users.noreply.github.com>
2024-07-14 22:34:35 +0600

    Add files via upload
    Week 16 , day 1, added

---
+++ Week 16/Day 1/A_Segment_Tree_for_the_Sum.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define NO cout << "NO\n"
+#define YES cout << "YES\n"
+// ll lcm(ll a, ll b) { return ((a / __gcd(a, b)) * b); }
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+    return 0;
+}
